3bf6fc3312738437deef82a49e48cef171d83aae
nya282828 <cat.route20@gmail.com>
2025-01-13 05:46:03 +0900

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vite';
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { globSync } from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const jsFiles = Object.fromEntries(
+  globSync('src/**/*.js', { ignore: ['node_modules/**','**/modules/**','**/dist/**']}).map(file => [
+    path.relative(
+      'src',
+      file.slice(0, file.length - path.extname(file).length)
+    ),
+    fileURLToPath(new URL(file, import.meta.url))
+  ])
+);
+
+const scssFiles = Object.fromEntries(
+  globSync('src/assets/styles/pages/**/*.scss', { ignore: ['src/assets/styles/pages/**/_*.scss'] }).map(file => [
+    path.relative(
+      'src',
+      file.slice(0, file.length - path.extname(file).length)
+    ),
+    fileURLToPath(new URL(file, import.meta.url))
+  ])
+);
+
+const htmlFiles = Object.fromEntries(
+  globSync('src/**/*.html', { ignore: ['node_modules/**', '**/dist/**'] }).map(file => [
+    path.relative(
+      'src',
+      file.slice(0, file.length - path.extname(file).length)
+    ),
+    fileURLToPath(new URL(file, import.meta.url))
+  ])
+);
+
+const inputObject = { ...scssFiles, ...jsFiles, ...htmlFiles };
+
+export default defineConfig({
+  root: './src',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: inputObject,
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      },
+   },
+  },
+  plugins: [
+    ViteEjsPlugin()
+  ],
+});
